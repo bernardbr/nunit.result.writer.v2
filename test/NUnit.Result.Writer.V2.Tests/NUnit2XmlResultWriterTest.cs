ae683af0ccc94419889794e16cc232887fec2075
Bernardo Esbérard <bernardbr@gmail.com>
2017-02-09 23:58:51 -0200

    Creates a empty test project.

---
+++ test/NUnit.Result.Writer.V2.Tests/NUnit2XmlResultWriterTest.cs
@@ -0,0 +1,14 @@
+namespace NUnit.Result.Writer.V2.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class NUnit2XmlResultWriterTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
