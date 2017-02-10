1fed267c6bd802459d02c6c42d33c66caec2cd4b
Bernardo Esbérard <bernardbr@gmail.com>
2017-02-09 23:47:35 -0200

    First commit.

---
+++ src/NUnit.Result.Writer.V2/Utils/SafeAttributeAccess.cs
@@ -0,0 +1,90 @@
+﻿// ***********************************************************************
+// Copyright (c) 2016 Charlie Poole
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
+
+namespace NUnit.Result.Writer.V2.Utils
+{
+    using System;
+    using System.Globalization;
+    using System.Xml;
+
+    /// <summary>
+    /// SafeAttributeAccess provides an extension method for accessing XML attributes.
+    /// </summary>
+    internal static class SafeAttributeAccess
+    {
+        /// <summary>
+        /// Gets the value of the given attribute.
+        /// </summary>
+        /// <param name="result">The result.</param>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public static string GetAttribute(this XmlNode result, string name)
+        {
+            XmlAttribute attr = result.Attributes[name];
+
+            return attr == null ? null : attr.Value;
+        }
+
+        /// <summary>
+        /// Gets the value of the given attribute as a double.
+        /// </summary>
+        /// <param name="result">The result.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns></returns>
+        public static double GetAttribute(this XmlNode result, string name, double defaultValue)
+        {
+            if (result.Attributes == null)
+            {
+                throw new ArgumentNullException(nameof(result), "The result don't have any attribute.");
+            }
+
+            var attr = result.Attributes[name];
+            return attr == null ? defaultValue : double.Parse(attr.Value, CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Gets the value of the given attribute as a DateTime.
+        /// </summary>
+        /// <param name="result">The result.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns></returns>
+        public static DateTime GetAttribute(this XmlNode result, string name, DateTime defaultValue)
+        {
+            var dateStr = GetAttribute(result, name);
+            if (dateStr == null)
+            {
+                return defaultValue;
+            }
+
+            DateTime date;
+            if (DateTime.TryParse(dateStr, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal | DateTimeStyles.AllowWhiteSpaces, out date))
+            {
+                return date;
+            }
+
+            return defaultValue;
+        }
+    }
+}
